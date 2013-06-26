7ed21303f05901fbae2bdc9210665ecbef3edd8a
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2013-06-26 16:28:06 +0200

    Initial entities update

--- src/Sylius/Bundle/CoreBundle/Model/Order.php
+++ src/Sylius/Bundle/CoreBundle/Model/Order.php
@@ -15,9 +15,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use FOS\UserBundle\Model\UserInterface;
 use Sylius\Bundle\AddressingBundle\Model\AddressInterface;
-use Sylius\Bundle\CoreBundle\Model\OrderInterface;
-use Sylius\Bundle\CoreBundle\Model\InventoryUnitInterface;
-use Sylius\Bundle\SalesBundle\Entity\Order as BaseOrder;
+use Sylius\Bundle\CartBundle\Model\Cart;
 use Sylius\Bundle\SalesBundle\Model\AdjustmentInterface;
 use Sylius\Bundle\ShippingBundle\Model\ShipmentInterface;
 
@@ -26,7 +24,7 @@ use Sylius\Bundle\ShippingBundle\Model\ShipmentInterface;
  *
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
  */
-class Order extends BaseOrder implements OrderInterface
+class Order extends Cart implements OrderInterface
 {
     /**
      * User.
@@ -351,10 +349,5 @@ class Order extends BaseOrder implements OrderInterface
         $this->currency = $currency;
 
         return $this;
-    }
-
-    public function setCreatedAt(\DateTime $createdAt)
-    {
-        $this->createdAt = $createdAt;
     }
 }
